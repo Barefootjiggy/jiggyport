c4a4d57868f18e2f9a3852c6e19c10ad9e6acdac
Josh <jpgiant@yahoo.com>
2024-06-17 09:45:51 -0400

    Pdf Icon actually downloads resume pdf instead of going to google docs

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,34 +1,36 @@
 import React from 'react';
 import { AppBar, Toolbar, BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 
 function Footer() {
   const [value, setValue] = React.useState(0);
-  const navigate = useNavigate();
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
     // Navigation logic based on icon click
     switch (newValue) {
-      case 0:
+      case 0: {
         // Open GitHub in a new tab
         window.open('https://github.com/Barefootjiggy', '_blank');
         break;
-      case 1:
-        // Open Resume (Google Docs) in a new tab
-        window.open('https://docs.google.com/document/d/1vQRjouMO8hj3-bxMZAhdh50PZOEU_vwuiRmx0lCePEs/edit', '_blank');
+      }
+      case 1: {
+        // Download PDF resume
+        const link = document.createElement('a');
+        link.href = 'https://docs.google.com/document/d/1vQRjouMO8hj3-bxMZAhdh50PZOEU_vwuiRmx0lCePEs/export?format=pdf'; // Direct PDF download link
+        link.download = 'Joshua_Pierre_Resume.pdf'; // The name for the downloaded file
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
         break;
-      case 2:
+      }
+      case 2: {
         // Open LinkedIn in a new tab
         window.open('https://www.linkedin.com/in/joshua-pierre-52209b91/', '_blank');
         break;
-      case 3:
-        // Direct navigation to Contact page
-        navigate('/contact');
-        break;
+      }
       default:
         break;
     }
