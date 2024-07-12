a14293a7f8250c91d2e817d43c7e5df9b4676e78
Josh <jpgiant@yahoo.com>
2024-07-11 21:25:51 -0400

    rotate on profile

--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -60,13 +60,23 @@ function Homepage() {
     config: { tension: 120, friction: 14 }
   });
 
+  const rotateProps = useSpring({
+    from: { transform: 'rotate(0deg)' },
+    to: { transform: 'rotate(360deg)' },
+    config: { duration: 3000 },
+    reset: true,
+    loop: { reverse: false },
+  });
+
   return (
     <animated.div style={slideInProps}>
       <ProfileBox>
         <CenteredTypography variant="h4" component="h1" gutterBottom m='20px auto'>
           Joshua Pierre
         </CenteredTypography>
-        <StyledAvatar src={Profilenobg} alt="Joshua Pierre" />
+        <animated.div style={rotateProps}>
+          <StyledAvatar src={Profilenobg} alt="Joshua Pierre" />
+        </animated.div>
         <CenteredTypography variant="h6" component="p" padding="0 20px">
           I am a full stack developer specializing in crafting solutions that harmoniously blend front-end aesthetics with back-end efficiency, delivering intuitive user experiences that users find both accessible and enjoyable. With a background in the healthcare industry creating medical authorizations my role required meticulous adherence to stringent HIPAA guidelines, ensuring the protection of sensitive patient information from unauthorized access and ensuring compliance with legal standards. I excel as a top performer, generating over 300+ authorizations monthly, while upholding stringent compliance and security standards. My experience as a Software Engineer has sharpened my time-management skills, enabling me to meet strict deadlines and deliver on projects. I am driven to continue to apply these skills in a company environment, aiming to become a top contributor.
         </CenteredTypography>
