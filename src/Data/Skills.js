64fca978ac5274f1cb55ac47575661e2cc14523c
Josh <jpgiant@yahoo.com>
2025-06-10 10:42:33 -0400

    import path update for skillimages

--- src/Data/Skills.js
+++ src/Data/Skills.js
@@ -1,18 +1,18 @@
-import Django from '../assets/Skillimages/Django.png'
-import Express from '../assets/Skillimages/Express.png'; 
-import MongoDB from '../assets/Skillimages/MongoDB.png'; 
-import Mongoose from '../assets/Skillimages/Mongoose.png'; 
-import Mui from '../assets/Skillimages/Mui.png'; 
-import PostgreSQL from '../assets/Skillimages/PostgreSQL.png'; 
-import Python from '../assets/Skillimages/Python.png'; 
-import RadixUI from '../assets/Skillimages/RadixUI.png'; 
-import ReactLogo from '../assets/Skillimages/React.png'; 
-import Nodejs from '../assets/Skillimages/Nodejs.png'; 
-import Css from '../assets/Skillimages/Css.png'; 
-import Html from '../assets/Skillimages/Html.png'; 
-import JS from '../assets/Skillimages/JS.png';
-import Wordpress from '../assets/Skillimages/Wordpress.png';
-import TypeScript from '../assets/Skillimages/TypeScript.png';
+import Django from '../assets/Skillimages/DjangoLogo.png'
+import Express from '../assets/Skillimages/ExpressLogo.png'; 
+import MongoDB from '../assets/Skillimages/MongoDBLogo.png'; 
+import Mongoose from '../assets/Skillimages/MongooseLogo.png'; 
+import Mui from '../assets/Skillimages/MuiLogo.png'; 
+import PostgreSQL from '../assets/Skillimages/PostgreSQLLogo.png'; 
+import Python from '../assets/Skillimages/PythonLogo.png'; 
+import RadixUI from '../assets/Skillimages/RadixUILogo.png'; 
+import ReactLogo from '../assets/Skillimages/ReactLogo.png'; 
+import Nodejs from '../assets/Skillimages/NodejsLogo.png'; 
+import Css from '../assets/Skillimages/CssLogo.png'; 
+import Html from '../assets/Skillimages/HtmlLogo.png'; 
+import JS from '../assets/Skillimages/JSLogo.png';
+import Wordpress from '../assets/Skillimages/WordpressLogo.png';
+import TypeScript from '../assets/Skillimages/TypeScriptLogo.png';
 
 export const skills = [
   { name: "Django", image: Django },
